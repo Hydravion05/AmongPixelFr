f404a760bad3f59786004ad4bf8cb536577753ef
Hydravion <94835595+Hydravion05@users.noreply.github.com>
2022-09-11 14:58:51 +0200

    commit all

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-const colorsChoice = document.querySelector('#colorsChoice')
-const game = document.querySelector('#game')
-const cursor = document.querySelector('#cursor')
-game.width = 1200
-game.height = 600
-const gridCellSize = 10
+const colorsChoice = document.querySelector('#colorsChoice');
+const game = document.querySelector('#game');
+const cursor = document.querySelector('#cursor');
+game.width = 1200;
+game.height = 600;
+const gridCellSize = 6;
+
+let toggle = document.querySelector('.toggle');
+let body = document.querySelector('body');
+
+toggle.addEventListener('click', function(){
+    body.classList.toggle('open');
+})
 
 const ctx = game.getContext('2d');
 const gridCtx = game.getContext('2d');
 
 const colorList = [
-    "#FFEBEE", "#FCE4EC", "#F3E5F5", "#B39DD8", "#9FA8DA", "#90CAF9", "#81D4FA", "#80DEEA", 
-    "#4DB6AC", "#66BB6A", "#9CCC65", "#CDDC39", "#FFEB3B", "#FFC107", "#FF9800", "#FF5722",
-    "#A1887F", "#E0E0E0", "#90A4AE", "#000"
+    "#FF4500", "#FFA800", "#FFD635", "#00A368", "#BE0039", "#FF3881", "#6D001A", "#FFF8B8", 
+    "#7EED56", "#2450A4", "#3690EA", "#51E9F4", "#00756F", "#009EAA", "#00CCC0", "#94B3FF",
+    "#811E9F", "#B44AC0", "#FF99AA", "#9C6926", "#493AC1", "#6A5CFF", "#E4ABFF", "#DE107F",
+    "#FFFFFF", "#D4D7D9", "#898D90", "#000000", "#00CC78", "#6D482F", "#FFB470", "#515252",
 ]
 let currentColorChoice = colorList[9]
 
@@ -50,7 +58,7 @@ function createPixel(x, y, color){
 }
 
 function addPixelIntoGame(){
-    const x = cursor.offsetLeft
+    const x = cursor.offsetLeft - game.offsetLeft
     const y = cursor.offsetTop - game.offsetTop
     
     createPixel(x, y, currentColorChoice)
@@ -75,7 +83,7 @@ game.addEventListener('click', function(){
 
 function drawGrids(ctx, width, height, cellWidth, cellHeight){
     ctx.beginPath()
-    ctx.strokeStyle = "#ccc"
+    ctx.strokeStyle = "transparent"
 
     for(let i = 0; i < width; i++){
         ctx.moveTo(i * cellWidth, 0)
